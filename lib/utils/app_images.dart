b8b262806e39745ce5bdd757f46189d75f373eac
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-22 16:32:24 +0530

    Add AppImages utility class for managing image paths

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  static const String _imgFolder = 'lib/assets/images';
+  static const String _logoSVG = '$_imgFolder/app_logo.svg';
+  static const String _appleSVG = '$_imgFolder/apple.svg';
+  static const String _deliveryBox = '$_imgFolder/delivery_box.png';
+  static String get logoSVG => _logoSVG;
+  static String get appleSVG => _appleSVG;
+  static String get deliveryBox => _deliveryBox;
+}
