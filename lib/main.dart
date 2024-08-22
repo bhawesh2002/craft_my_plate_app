078733407c7efb5ca988c7cd155761a831fe152f
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-22 19:44:00 +0530

    chore: Update main.dart to set system UI overlay style

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,19 @@ import 'package:craft_my_plate_app/routes/app_routes.dart';
 import 'package:craft_my_plate_app/utils/app_colors.dart';
 import 'package:craft_my_plate_app/utils/ui_sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
   runApp(const MainApp());
 }
 
