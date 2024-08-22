9a56bb4de635fe606f2cf46c16ef08a20edfc22a
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-22 19:31:02 +0530

    feat: Add background image support in AppImages utility class

--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -3,7 +3,10 @@ class AppImages {
   static const String _logoSVG = '$_imgFolder/app_logo.svg';
   static const String _appleSVG = '$_imgFolder/apple.svg';
   static const String _deliveryBox = '$_imgFolder/delivery_box.png';
+  static const String _bgSVG = '$_imgFolder/background.svg';
+
   static String get logoSVG => _logoSVG;
   static String get appleSVG => _appleSVG;
   static String get deliveryBox => _deliveryBox;
+  static String get bgSVG => _bgSVG;
 }
