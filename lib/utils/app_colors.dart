b98fd5f4dd360fe1c28271e9b6d70ffe712d55b4
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-22 19:30:54 +0530

    chore: Add courgetteTextColor to AppColors

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primary = Color(0xff6318AF);
   static const Color secondary = Color(0xffF7E5B7);
+  static const Color courgetteTextColor = Color(0xffD5B67D);
 }
