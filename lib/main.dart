a2849eb936379deb9d90f6be37b8319838e9b5b4
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-22 17:53:07 +0530

    feat: Integrate Get package for route management

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:craft_my_plate_app/routes/app_pages.dart';
+import 'package:craft_my_plate_app/routes/app_routes.dart';
 import 'package:craft_my_plate_app/utils/app_colors.dart';
 import 'package:craft_my_plate_app/utils/ui_sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,7 +16,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // Initialize the UiSizes class
     UiSizes().init(context);
-    return MaterialApp(
+    return GetMaterialApp(
       theme: ThemeData(
         colorScheme: const ColorScheme.light(
           primary: AppColors.primary,
@@ -21,6 +24,8 @@ class MainApp extends StatelessWidget {
         ),
         fontFamily: 'Lexend',
       ),
+      initialRoute: AppRoutes.splash,
+      getPages: AppPages.pages,
       home: const Scaffold(body: Center()),
     );
   }
