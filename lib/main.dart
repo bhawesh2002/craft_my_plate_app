bc56be8856bde6fa948d9968544e5e08483b1bce
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-23 07:01:26 +0530

    feat: Initialize Firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:craft_my_plate_app/firebase_options.dart';
 import 'package:craft_my_plate_app/routes/app_pages.dart';
 import 'package:craft_my_plate_app/routes/app_routes.dart';
 import 'package:craft_my_plate_app/utils/app_colors.dart';
 import 'package:craft_my_plate_app/utils/ui_sizes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
