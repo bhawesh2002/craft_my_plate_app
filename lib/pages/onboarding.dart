96843d1dbbe21bd7c46ea8a2139c6ab6110341a0
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-23 21:44:55 +0530

    feat: Add lazy loading for images and smooth page indicator to onboarding page

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -2,9 +2,11 @@ import 'package:craft_my_plate_app/routes/app_routes.dart';
 import 'package:craft_my_plate_app/utils/app_colors.dart';
 import 'package:craft_my_plate_app/utils/app_images.dart';
 import 'package:craft_my_plate_app/utils/ui_sizes.dart';
+import 'package:craft_my_plate_app/widgets/get_started_button.dart';
 import 'package:craft_my_plate_app/widgets/onboarding_card.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
@@ -113,7 +115,7 @@ class _OnboardingState extends State<Onboarding> {
                     builder: (context, constraints) {
                       return PageView.builder(
                         controller: _pageController,
-                        physics: const NeverScrollableScrollPhysics(),
+                        // physics: const NeverScrollableScrollPhysics(),
                         itemCount: onboardingCards.length,
                         itemBuilder: (context, index) {
                           return onboardingCards[index];
@@ -123,22 +125,16 @@ class _OnboardingState extends State<Onboarding> {
                   ),
                 ),
               ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: List.generate(onboardingCards.length, (index) {
-                  return Container(
-                    constraints: const BoxConstraints(maxWidth: 28),
-                    width: 28,
-                    height: 10,
-                    margin: const EdgeInsets.symmetric(horizontal: 10.0),
-                    decoration: BoxDecoration(
-                      color: currentIndex == index
-                          ? AppColors.buttonPrimaryColor
-                          : AppColors.buttonSecondaryColor,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  );
-                }),
+              SmoothPageIndicator(
+                controller: _pageController,
+                count: 3,
+                effect: const SwapEffect(
+                  dotColor: AppColors.buttonSecondaryColor,
+                  activeDotColor: AppColors.buttonPrimaryColor,
+                  dotHeight: 12,
+                  dotWidth: 30,
+                  spacing: 16,
+                ),
               ),
               SizedBox(height: UiSizes().h5),
               GestureDetector(
@@ -151,70 +147,11 @@ class _OnboardingState extends State<Onboarding> {
                         curve: Curves.easeIn);
                   }
                 },
-                child: Container(
-                  padding: const EdgeInsets.all(14),
-                  decoration: BoxDecoration(
-                    color: AppColors.buttonSecondaryColor,
-                    borderRadius: BorderRadius.circular(64),
-                  ),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      // This controls the slide-in and opacity animation of the "Get Started" text
-                      AnimatedSwitcher(
-                        duration: const Duration(milliseconds: 300),
-                        transitionBuilder: (clild, animation) {
-                          return SlideTransition(
-                            position: Tween<Offset>(
-                                    begin: const Offset(0.5, 0),
-                                    end: Offset.zero)
-                                .animate(animation),
-                            child: clild,
-                          );
-                        },
-                        child: (currentIndex == onboardingCards.length - 1)
-                            ? AnimatedOpacity(
-                                opacity:
-                                    currentIndex == onboardingCards.length - 1
-                                        ? 1
-                                        : 0,
-                                duration: const Duration(milliseconds: 300),
-                                child: AnimatedSlide(
-                                  offset:
-                                      currentIndex == onboardingCards.length - 1
-                                          ? Offset.zero
-                                          : const Offset(0.5, 0),
-                                  duration: const Duration(milliseconds: 300),
-                                  child: const Padding(
-                                    padding:
-                                        EdgeInsets.symmetric(horizontal: 18.0),
-                                    child: Text(
-                                      "Get Started",
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        color: AppColors.primary,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              )
-                            : const SizedBox(),
-                      ),
-
-                      Container(
-                        padding: const EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: AppColors.buttonPrimaryColor,
-                          border: Border.all(color: Colors.white, width: 2),
-                        ),
-                        child: const Icon(Icons.arrow_forward,
-                            color: Colors.white),
-                      ),
-                    ],
-                  ),
+                child: GetStartedButton(
+                  pageIndex: currentIndex,
+                  triggerPageIndex: onboardingCards.length - 1,
                 ),
-              )
+              ),
             ],
           ),
         ),
