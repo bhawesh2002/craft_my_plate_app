36065d6ebe8fb6ae633521e33c61c2b41c7420a1
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-25 18:23:36 +0530

    feat: Update MealBoxCard widget with customizable image position and scale
    This commit adds new optional parameters to the MealBoxCard widget, allowing developers to customize the position and scale of the image within the card. The `topPos` parameter sets the top position of the image, defaulting to 0.1. The `leftPos` parameter sets the left position of the image, defaulting to 0.1. The `angle` parameter sets the angle of the image, defaulting to 0. The `scale` parameter sets the scale of the image, defaulting to 1.1. These enhancements provide more flexibility in designing the layout and appearance of the MealBoxCard widget.

--- lib/widgets/meal_box_card.dart
+++ lib/widgets/meal_box_card.dart
@@ -6,49 +6,66 @@ class MealBoxCard extends StatelessWidget {
   final String imgPath;
   final int count;
   final LinearGradient gradient;
+
+  /// The top position of the image in the card Defaults to 0.1
+  final double? topPos;
+
+  /// The left position of the image in the card Defaults to 0.1
+  final double? leftPos;
+
+  /// The angle of the image in the card Defaults to 0
+  final double? angle;
+
+  /// The scale of the image in the card Defaults to 1.1
+  final double? scale;
   const MealBoxCard(
       {super.key,
       required this.imgPath,
       required this.count,
-      required this.gradient});
+      required this.gradient,
+      this.topPos,
+      this.leftPos,
+      this.angle,
+      this.scale});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: UiSizes().w50,
-      height: UiSizes().h15,
-      margin: const EdgeInsets.all(6),
-      clipBehavior: Clip.antiAlias,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(32),
-        gradient: gradient,
-      ),
-      child: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            left: 50,
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: Transform.translate(
-                offset: const Offset(0, -20),
-                child: Transform.scale(
-                  scale: 1.1,
-                  child: Image.asset(
-                    imgPath,
+        width: UiSizes().w50,
+        height: UiSizes().h15,
+        margin: const EdgeInsets.all(6),
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(32),
+          gradient: gradient,
+        ),
+        child: LayoutBuilder(builder: (context, constraints) {
+          return Stack(
+            children: [
+              Positioned(
+                top: constraints.maxHeight * (topPos ?? 0.1),
+                left: constraints.maxWidth * (leftPos ?? 0.1),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Transform.rotate(
+                    angle: angle ?? 0,
+                    child: Transform.scale(
+                      scale: scale ?? 1.1,
+                      child: Image.asset(
+                        imgPath,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ),
-          Positioned(
-            bottom: 25,
-            child: Align(
-                alignment: Alignment.centerLeft,
-                child: ItemLabel(count: count)),
-          ),
-        ],
-      ),
-    );
+              Positioned(
+                bottom: 25,
+                child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: ItemLabel(count: count, label: 'Items Box')),
+              ),
+            ],
+          );
+        }));
   }
 }
