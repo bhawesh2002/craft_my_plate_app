bdbb4e2e8a495e6544736ba2eb294083213fa3cb
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-25 15:08:18 +0530

    feat: Add SVG icons for veg indicator and sparkles

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,8 @@ import 'package:craft_my_plate_app/utils/ui_sizes.dart';
 import 'package:craft_my_plate_app/widgets/coupon_card.dart';
 import 'package:craft_my_plate_app/widgets/craft_card_long.dart';
 import 'package:craft_my_plate_app/widgets/craft_card_short.dart';
+import 'package:craft_my_plate_app/widgets/how_it_works.dart';
+import 'package:craft_my_plate_app/widgets/location_widget.dart';
 import 'package:craft_my_plate_app/widgets/meal_box_card.dart';
 import 'package:craft_my_plate_app/widgets/menu_card_large.dart';
 import 'package:craft_my_plate_app/widgets/section_tile.dart';
@@ -37,188 +39,194 @@ class _HomePageState extends State<HomePage> {
     ];
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.only(
-            top: MediaQuery.of(context).padding.top, left: 24, right: 24),
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
         child: SingleChildScrollView(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(height: 24),
-              Text(
-                'Hi, ${_auth.currentUser!.displayName}!',
-                style: const TextStyle(fontSize: 22, color: AppColors.primary),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Text(
+                  'Hi, ${_auth.currentUser!.displayName}!',
+                  style:
+                      const TextStyle(fontSize: 22, color: AppColors.primary),
+                ),
               ),
               const SizedBox(height: 12),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Current location',
-                        style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.w300,
-                            color: AppColors.lightTextColor),
-                      ),
-                      const SizedBox(height: 4),
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Icon(
-                            Icons.pin_drop_outlined,
-                            color: AppColors.primary,
-                          ),
-                          const SizedBox(width: 4),
-                          const Text(
-                            "Hyderabad",
-                            style: TextStyle(fontSize: 14),
-                          ),
-                          const SizedBox(width: 4),
-                          Transform.rotate(
-                            angle: 3.14 / 2, // 90 degrees
-                            child: const Icon(
-                              Icons.arrow_forward_ios,
-                              size: 16,
-                              color: Color(0xffAAAAAA),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  const Column(
-                    children: [
-                      Icon(
-                        Icons.play_circle_rounded,
-                        color: AppColors.primary,
-                      ),
-                      SizedBox(height: 4),
-                      Text(
-                        "How it works?",
-                        style: TextStyle(
-                            fontSize: 10, fontWeight: FontWeight.w300),
-                      )
-                    ],
-                  )
-                ],
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [LocationWidget(), HowItWorks()],
+                ),
               ),
               const SizedBox(height: 32),
-              SizedBox(
-                height: UiSizes().h20,
-                child: PageView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 2,
-                  itemBuilder: (context, index) {
-                    return const CouponCard();
-                  },
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: SizedBox(
+                  height: UiSizes().h20,
+                  child: PageView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 2,
+                    itemBuilder: (context, index) {
+                      return const CouponCard();
+                    },
+                  ),
                 ),
               ),
               const SizedBox(height: 24),
-              const Text(
-                "Start Crafting",
-                style: TextStyle(
-                  fontSize: 20,
-                  height: 1.5,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Column(
-                children: [
-                  const CraftCardLong(),
-                  const SizedBox(height: 12),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Expanded(
-                          child: CraftCardShort(
-                        title: "Meal Box",
-                        guestNumber: 10,
-                        color: const Color(0xff6318AF),
-                        imgPath: AppImages.mealBox,
-                        cardBgColor: const Color(0xffFBF7FE),
-                      )),
-                      const SizedBox(width: 12),
-                      Expanded(
-                          child: CraftCardShort(
-                        title: "Catering",
-                        guestNumber: 120,
-                        color: const Color(0xffE70472),
-                        imgPath: AppImages.biryani,
-                        cardBgColor: const Color(0xffFBDAEA),
-                      )),
-                    ],
-                  )
-                ],
-              ),
-              const SizedBox(height: 24),
-              Text(
-                "Choose Your Platter",
-                style: TextStyle(
-                    fontSize: 18,
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "Start Crafting",
+                  style: TextStyle(
+                    fontSize: 20,
                     height: 1.5,
-                    color: const Color(0xff242628),
-                    fontWeight: FontWeight.w500,
-                    shadows: [
-                      BoxShadow(
-                        offset: const Offset(0, 4),
-                        color: Colors.black.withOpacity(0.25),
-                        blurRadius: 4,
-                      ),
-                    ]),
-              ),
-              const SizedBox(height: 24),
-              const SectionTile(
-                title: "Delivery Box",
-                quantity: "Min 10 - Max 50",
-                description: "Best for small gatherings and house-parties",
-                trailingText: "MORE",
+                  ),
+                ),
               ),
               const SizedBox(height: 20),
-              const SingleChildScrollView(
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  children: [
+                    const CraftCardLong(),
+                    const SizedBox(height: 12),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Expanded(
+                          child: CraftCardShort(
+                            title: "Meal Box",
+                            guestNumber: 10,
+                            color: const Color(0xff6318AF),
+                            imgPath: AppImages.mealBox,
+                            cardBgColor: const Color(0xffFBF7FE),
+                          ),
+                        ),
+                        const SizedBox(width: 12),
+                        Expanded(
+                          child: CraftCardShort(
+                            title: "Catering",
+                            guestNumber: 120,
+                            color: const Color(0xffE70472),
+                            imgPath: AppImages.biryani,
+                            cardBgColor: const Color(0xffFBDAEA),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 24),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Text(
+                  "Choose Your Platter",
+                  style: TextStyle(
+                      fontSize: 18,
+                      height: 1.5,
+                      color: const Color(0xff242628),
+                      fontWeight: FontWeight.w500,
+                      shadows: [
+                        BoxShadow(
+                          offset: const Offset(0, 4),
+                          color: Colors.black.withOpacity(0.25),
+                          blurRadius: 4,
+                        ),
+                      ]),
+                ),
+              ),
+              const SizedBox(height: 24),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: SectionTile(
+                  title: "Delivery Box",
+                  quantity: "Min 10 - Max 50",
+                  description: "Best for small gatherings and house-parties",
+                  trailingText: "MORE",
+                ),
+              ),
+              const SizedBox(height: 20),
+              SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(
-                  children: [MenuCardLarge(), MenuCardLarge(), MenuCardLarge()],
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: UiSizes().w4),
+                      child: const MenuCardLarge(),
+                    ),
+                    const MenuCardLarge(),
+                    Padding(
+                        padding: EdgeInsets.only(right: UiSizes().w4),
+                        child: const MenuCardLarge()),
+                  ],
                 ),
               ),
               const SizedBox(height: 24),
-              const SectionTile(
-                title: "Meal Box",
-                quantity: "Min 10",
-                description: "Individually packed meal boxes of happiness!",
-                trailingText: "MORE",
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: SectionTile(
+                  title: "Meal Box",
+                  quantity: "Min 10",
+                  description: "Individually packed meal boxes of happiness!",
+                  trailingText: "MORE",
+                ),
               ),
               const SizedBox(height: 20),
               SizedBox(
                 height: UiSizes().h16,
-                width: UiSizes().w90,
                 child: ListView.builder(
                   scrollDirection: Axis.horizontal,
                   itemCount: mealBoxCards.length,
+                  padding: EdgeInsets.zero,
                   itemBuilder: (context, index) {
+                    if (index == 0) {
+                      return Padding(
+                        padding: EdgeInsets.only(left: UiSizes().w4),
+                        child: mealBoxCards[index],
+                      );
+                    }
+                    if (index == mealBoxCards.length - 1) {
+                      return Padding(
+                        padding: EdgeInsets.only(right: UiSizes().w4),
+                        child: mealBoxCards[index],
+                      );
+                    }
                     return mealBoxCards[index];
                   },
                 ),
               ),
               const SizedBox(height: 24),
-              const SectionTile(
-                title: "Catering Menus",
-                quantity: "Min 200",
-                description:
-                    "Best for weddings, Corporate Events, Birthdays etc",
-                trailingText: "",
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: SectionTile(
+                  title: "Catering Menus",
+                  quantity: "Min 200",
+                  description:
+                      "Best for weddings, Corporate Events, Birthdays etc",
+                  trailingText: "",
+                ),
               ),
               const SizedBox(
                 height: 20,
               ),
-              const SingleChildScrollView(
+              SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(
-                  children: [MenuCardLarge(), MenuCardLarge(), MenuCardLarge()],
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: UiSizes().w4),
+                      child: const MenuCardLarge(),
+                    ),
+                    const MenuCardLarge(),
+                    Padding(
+                        padding: EdgeInsets.only(right: UiSizes().w4),
+                        child: const MenuCardLarge()),
+                  ],
                 ),
               ),
             ],
