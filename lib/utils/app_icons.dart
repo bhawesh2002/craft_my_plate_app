cd92bff981ffb2309906f4980c90c529bf02b528
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-25 14:45:39 +0530

    feat: Add AppIcons class for managing app icons
    This commit adds a new class called AppIcons to manage the app icons. It includes static variables for the paths of various icons used in the app. This enhancement improves code organization and makes it easier to access and use the app icons throughout the codebase.

---
+++ lib/utils/app_icons.dart
@@ -0,0 +1,11 @@
+class AppIcons {
+  static const String _mapPin = 'lib/assets/icons/map_pin.svg';
+  static const String _sparkles = 'lib/assets/icons/sparkles.svg';
+  static const String _playCircle = 'lib/assets/icons/play_circle.svg';
+  static const String _vegIndicator = 'lib/assets/icons/veg_indicator.svg';
+
+  static String get mapPin => _mapPin;
+  static String get sparkles => _sparkles;
+  static String get playCircle => _playCircle;
+  static String get vegIndicator => _vegIndicator;
+}
