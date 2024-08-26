077f146b14c32e6fd96379d1461f0fd0615635ea
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-26 17:22:30 +0530

    feat: Update image assets and references
    This commit updates the image assets and their references in the codebase. It removes unused image assets and updates the paths of existing assets. This change improves the organization and maintainability of the project.

--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -1,43 +1,16 @@
 class AppImages {
   static const String _imgFolder = 'lib/assets/images';
-  static const String _lottieFolder = 'lib/assets/lottie';
-  static const String _logoSVG = '$_imgFolder/app_logo.svg';
-  static const String _logoCircleSVG = '$_imgFolder/logoCircle.svg';
-  static const String _appleSVG = '$_imgFolder/apple.svg';
   static const String _deliveryBox = '$_imgFolder/delivery_box.png';
-  static const String _bgSVG = '$_imgFolder/background.svg';
-  static const String _onboarding1 = '$_imgFolder/onboarding_1_img.svg';
-  static const String _onboarding2 = '$_imgFolder/onboarding_2_img.svg';
-  static const String _onboarding3 = '$_imgFolder/onboarding_3_img.svg';
-  static const String _onboarding1Lottie = '$_lottieFolder/onboarding_1.json';
-  static const String _onboarding2Lottie = '$_lottieFolder/onboarding_2.json';
-  static const String _onboarding3Lottie = '$_lottieFolder/onboarding_3.json';
-  static const String _elephantSVG = '$_imgFolder/elephant_cook.svg';
-  static const String _cookingSVG = '$_imgFolder/cooking.svg';
   static const String _mealBox = '$_imgFolder/meal_box.png';
   static const String _mealBoxSmall = '$_imgFolder/meal_box_small.png';
   static const String _mealBoxLarge = '$_imgFolder/meal_box_large.png';
   static const String _biryani = '$_imgFolder/biryani.png';
   static const String _specialMenu = '$_imgFolder/special_menu.png';
-  static const String _arrowHeadSVG = '$_imgFolder/arrowHead.svg';
 
-  static String get logoSVG => _logoSVG;
-  static String get appleSVG => _appleSVG;
   static String get deliveryBox => _deliveryBox;
-  static String get bgSVG => _bgSVG;
-  static String get onboarding1 => _onboarding1;
-  static String get onboarding2 => _onboarding2;
-  static String get onboarding3 => _onboarding3;
-  static String get onboarding1Lottie => _onboarding1Lottie;
-  static String get onboarding2Lottie => _onboarding2Lottie;
-  static String get onboarding3Lottie => _onboarding3Lottie;
-  static String get elephantSVG => _elephantSVG;
-  static String get cookingSVG => _cookingSVG;
   static String get mealBox => _mealBox;
   static String get mealBoxSmall => _mealBoxSmall;
   static String get mealBoxLarge => _mealBoxLarge;
   static String get biryani => _biryani;
   static String get specialMenu => _specialMenu;
-  static String get arrowHeadSVG => _arrowHeadSVG;
-  static String get logoCircleSVG => _logoCircleSVG;
 }
---
+++ lib/utils/app_svgs.dart
@@ -0,0 +1,18 @@
+class AppSvgs {
+  static const String _svgFolder = 'lib/assets/svgs';
+  static const String _logoSVG = '$_svgFolder/app_logo.svg';
+  static const String _logoCircleSVG = '$_svgFolder/logoCircle.svg';
+  static const String _appleSVG = '$_svgFolder/apple.svg';
+  static const String _bgSVG = '$_svgFolder/background.svg';
+  static const String _elephantSVG = '$_svgFolder/elephant_cook.svg';
+  static const String _cookingSVG = '$_svgFolder/cooking.svg';
+  static const String _arrowHeadSVG = '$_svgFolder/arrowHead.svg';
+
+  static String get logoSVG => _logoSVG;
+  static String get logoCircleSVG => _logoCircleSVG;
+  static String get appleSVG => _appleSVG;
+  static String get bgSVG => _bgSVG;
+  static String get elephantSVG => _elephantSVG;
+  static String get cookingSVG => _cookingSVG;
+  static String get arrowHeadSVG => _arrowHeadSVG;
+}
