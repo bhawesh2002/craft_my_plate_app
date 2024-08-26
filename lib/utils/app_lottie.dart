df9161c04c02f9f26609f736fa2d99a4c57298a8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-26 17:22:49 +0530

    feat: Add AppLottie utility class for managing Lottie animation assets
    This commit adds a new utility class called AppLottie to the lib/utils/app_lottie.dart file. The AppLottie class provides static constants for storing the paths of Lottie animation assets used in the application's onboarding process. The class includes getters for accessing the paths of three specific animations: choose_n_customise.json, order_more_save_more.json, and personal_order_exec.json. This addition improves the organization and maintainability of the codebase by centralizing the management of Lottie animation assets.

---
+++ lib/utils/app_lottie.dart
@@ -0,0 +1,13 @@
+class AppLottie {
+  static const String _lottieFolder = 'lib/assets/lottie';
+  static const String _onboarding1Lottie =
+      '$_lottieFolder/choose_n_customise.json';
+  static const String _onboarding2Lottie =
+      '$_lottieFolder/order_more_save_more.json';
+  static const String _onboarding3Lottie =
+      '$_lottieFolder/personal_order_exec.json';
+
+  static String get onboarding1Lottie => _onboarding1Lottie;
+  static String get onboarding2Lottie => _onboarding2Lottie;
+  static String get onboarding3Lottie => _onboarding3Lottie;
+}
