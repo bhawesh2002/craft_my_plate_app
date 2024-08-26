67be96a1fe141c29b52c4ec712e45411bcf80834
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-26 13:09:07 +0530

    feat: Add CmpBottomAppBar widget for displaying a custom bottom app bar
    This commit adds a new widget called CmpBottomAppBar, which represents a custom bottom app bar. The widget is implemented in the lib/widgets/cmp_bottom_app_bar.dart file and provides a container with rounded corners, a white background color, and a shadow effect. Inside the container, there is a row of icons and labels representing different sections of the application, such as "Profile" and "Orders". The widget enhances the visual appeal and user experience of the application by providing a consistent and intuitive navigation option at the bottom of the screen.

---
+++ lib/widgets/cmp_bottom_app_bar.dart
@@ -0,0 +1,71 @@
+import 'package:craft_my_plate_app/utils/app_colors.dart';
+import 'package:craft_my_plate_app/utils/app_icons.dart';
+import 'package:craft_my_plate_app/utils/app_images.dart';
+import 'package:craft_my_plate_app/utils/ui_sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CmpBottomAppBar extends StatefulWidget {
+  const CmpBottomAppBar({super.key});
+
+  @override
+  State<CmpBottomAppBar> createState() => _CmpBottomAppBarState();
+}
+
+class _CmpBottomAppBarState extends State<CmpBottomAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    final UiSizes uiSizes = UiSizes();
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(width: uiSizes.w4),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SvgPicture.asset(AppIcons.userCircle),
+              Text(
+                "Profile",
+                style: TextStyle(
+                  fontSize: uiSizes.responsiveFontSize(12),
+                  color: AppColors.lightTextColor,
+                ),
+              )
+            ],
+          ),
+          SizedBox(width: uiSizes.w6),
+          Transform.scale(
+              scale: 1.2, child: SvgPicture.asset(AppImages.logoCircleSVG)),
+          SizedBox(width: uiSizes.w6),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SvgPicture.asset(AppIcons.dinnerPlate),
+              Text(
+                "Orders",
+                style: TextStyle(
+                  fontSize: uiSizes.responsiveFontSize(12),
+                  color: AppColors.lightTextColor,
+                ),
+              )
+            ],
+          ),
+          SizedBox(width: uiSizes.w4),
+        ],
+      ),
+    );
+  }
+}
